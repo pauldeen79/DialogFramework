31215d37e699fc0ed31861cad625b617cded2dd4
Paul Deen <paul.deen@dutchworkz.nl>
2022-04-27 16:41:55 +0200

    Added result values for Date, DateTime, Number and Text

---
+++ src/DialogFramework.Core/DomainModel/DialogPartResultValues/DateTimeDialogPartResultValue.cs
@@ -0,0 +1,8 @@
+﻿namespace DialogFramework.Core.DomainModel.DialogPartResultValues;
+
+public record DateTimeDialogPartResultValue : IDialogPartResultValue
+{
+    public DateTimeDialogPartResultValue(DateTime value) => Value = value;
+    public object? Value { get; }
+    public ResultValueType ResultValueType => ResultValueType.DateTime;
+}
