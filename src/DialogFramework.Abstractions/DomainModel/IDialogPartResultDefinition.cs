8fccc4891abc2bddc027c08a299e0d6c6bfbca06
Paul Deen <paul.deen@dutchworkz.nl>
2022-04-30 15:31:39 +0200

    Added Validators to DialogPartResultDefinition, so you can add client-side validation before posting values

--- src/DialogFramework.Abstractions/DomainModel/IDialogPartResultDefinition.cs
+++ src/DialogFramework.Abstractions/DomainModel/IDialogPartResultDefinition.cs
@@ -5,5 +5,6 @@ public interface IDialogPartResultDefinition
     string Id { get; }
     string Title { get; }
     ResultValueType ValueType { get; }
+    ValueCollection<IDialogPartResultDefinitionValidator> Validators { get; }
     IEnumerable<ValidationResult> Validate(ValidationContext validationContext, IDialogPartResult dialogPartResult);
 }
--- src/DialogFramework.Core/DomainModel/DialogPartResultDefinitions/EmptyDialogPartResultDefinition.cs
+++ src/DialogFramework.Core/DomainModel/DialogPartResultDefinitions/EmptyDialogPartResultDefinition.cs
@@ -6,6 +6,8 @@ public record EmptyDialogPartResultDefinition : IDialogPartResultDefinition
     public string Title => string.Empty;
     public ResultValueType ValueType => ResultValueType.None;
 
+    public ValueCollection<IDialogPartResultDefinitionValidator> Validators => new();
+
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext, IDialogPartResult dialogPartResult)
         => Enumerable.Empty<ValidationResult>();
 }
