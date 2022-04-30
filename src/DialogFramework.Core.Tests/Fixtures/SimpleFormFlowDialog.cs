2dc730b1acd995915172147ccf6fcadd4a738350
Paul Deen <paul.deen@dutchworkz.nl>
2022-04-30 13:06:26 +0200

    Refactored test dialogs by using inheritance, which removes duplicate code

--- src/DialogFramework.Core.Tests/Fixtures/SimpleFormFlowDialog.cs
+++ src/DialogFramework.Core.Tests/Fixtures/SimpleFormFlowDialog.cs
@@ -1,54 +1,33 @@
 ﻿namespace DialogFramework.Core.Tests.Fixtures;
 
-public record SimpleFormFlowDialog : IDialog
+public record SimpleFormFlowDialog : Dialog
 {
-    public IDialogMetadata Metadata => new DialogMetadata(nameof(TestFlowDialog), "Simple fom flow dialog", "1.0.0", true);
+    private static readonly IDialogPartGroup GetInformationGroup = new DialogPartGroup("Get information", "Get information", 1);
+    private static readonly IDialogPartGroup CompletedGroup = new DialogPartGroup("Completed", "Completed", 2);
 
-    public ValueCollection<IDialogPart> Parts { get; }
-    public IErrorDialogPart ErrorPart { get; }
-    public IAbortedDialogPart AbortedPart { get; }
-    public ICompletedDialogPart CompletedPart { get; }
-    public ValueCollection<IDialogPartGroup> PartGroups { get; }
-
-    public SimpleFormFlowDialog()
+    public SimpleFormFlowDialog() : base(
+        new DialogMetadata(nameof(TestFlowDialog), "Simple fom flow dialog", "1.0.0", true),
+        new IDialogPart[]
+        {
+            new AllRequiredQuestionDialogPart
+            (
+                "ContactInfo",
+                "Contact information",
+                "Please provide your e-mail address and telephone number, so we can contact you.",
+                GetInformationGroup,
+                new IDialogPartResultDefinition[]
+                {
+                    new DialogPartResultDefinition("EmailAddress", "E-mail address", ResultValueType.Text),
+                    new DialogPartResultDefinition("TelephoneNumber", "Telephone number", ResultValueType.Text),
+                    new DialogPartResultDefinition("SignUpForNewsletter", "Subscribe to newsletter (optional)", ResultValueType.YesNo)
+                }
+            )
+        },
+        new ErrorDialogPart("Error", "Something went wrong. Please try again, or contact us in case the problem persists.", null),
+        new AbortedDialogPart("Aborted", "The dialog is aborted. You can come back any time to start the application again."),
+        new CompletedDialogPart("Completed", "Completed", "Thank you for using this application. Please come back soon!", CompletedGroup),
+        new[] { GetInformationGroup, CompletedGroup }
+        )
     {
-        var getInformationGroup = new DialogPartGroup("Get information", "Get information", 1);
-        var completedGroup = new DialogPartGroup("Completed", "Completed", 2);
-        Parts = new ValueCollection<IDialogPart>
-        (
-            new IDialogPart[]
-            {
-                new AllRequiredQuestionDialogPart
-                (
-                    "ContactInfo",
-                    "Contact information",
-                    "Please provide your e-mail address and telephone number, so we can contact you.",
-                    getInformationGroup,
-                    new IDialogPartResultDefinition[]
-                    {
-                        new DialogPartResultDefinition("EmailAddress", "E-mail address", ResultValueType.Text),
-                        new DialogPartResultDefinition("TelephoneNumber", "Telephone number", ResultValueType.Text),
-                        new DialogPartResultDefinition("SignUpForNewsletter", "Subscribe to newsletter (optional)", ResultValueType.YesNo)
-                    }
-                )
-            }
-        );
-        ErrorPart = new ErrorDialogPart("Error", "Something went wrong. Please try again, or contact us in case the problem persists.", null);
-        AbortedPart = new AbortedDialogPart("Aborted", "The dialog is aborted. You can come back any time to start the application again.");
-        CompletedPart = new CompletedDialogPart("Completed", "Completed", "Thank you for using this application. Please come back soon!", completedGroup);
-        PartGroups = new ValueCollection<IDialogPartGroup>(new[] { getInformationGroup, completedGroup });
     }
-
-    public IEnumerable<IDialogPartResult> ReplaceAnswers(IEnumerable<IDialogPartResult> existingDialogPartResults,
-                                                         IEnumerable<IDialogPartResult> newDialogPartResults)
-    {
-        var dialogPartIds = newDialogPartResults.GroupBy(x => x.DialogPartId).Select(x => x.Key).ToArray();
-        return existingDialogPartResults.Where(x => !dialogPartIds.Contains(x.DialogPartId)).Concat(newDialogPartResults);
-    }
-
-    public IEnumerable<IDialogPartResult> ResetDialogPartResultByPart(IEnumerable<IDialogPartResult> existingDialogPartResults, IDialogPart currentPart)
-        => existingDialogPartResults.Where(x => x.DialogPartId != currentPart.Id);
-
-    public bool CanNavigateTo(IDialogPart currentPart, IDialogPart navigateToPart, IEnumerable<IDialogPartResult> existingDialogPartResults)
-        => currentPart.Id == navigateToPart.Id || existingDialogPartResults.Any(x => x.DialogPartId == navigateToPart.Id);
 }
--- src/DialogFramework.Core.Tests/Fixtures/TestFlowDialog.cs
+++ src/DialogFramework.Core.Tests/Fixtures/TestFlowDialog.cs
@@ -1,89 +1,68 @@
 ﻿namespace DialogFramework.Core.Tests.Fixtures;
 
-public record TestFlowDialog : IDialog
+public record TestFlowDialog : Dialog
 {
-    public IDialogMetadata Metadata => new DialogMetadata(nameof(TestFlowDialog), "Test flow dialog", "1.0.0", true);
+    private static readonly IDialogPartGroup WelcomeGroup = new DialogPartGroup("Welcome", "Welcome", 1);
+    private static readonly IDialogPartGroup GetInformationGroup = new DialogPartGroup("Get information", "Get information", 2);
+    private static readonly IDialogPartGroup CompletedGroup = new DialogPartGroup("Completed", "Completed", 3);
+    private const string EmailPartId = "Email";
 
-    public ValueCollection<IDialogPart> Parts { get; }
-    public IErrorDialogPart ErrorPart { get; }
-    public IAbortedDialogPart AbortedPart { get; }
-    public ICompletedDialogPart CompletedPart { get; }
-    public ValueCollection<IDialogPartGroup> PartGroups { get; }
-
-    public TestFlowDialog()
+    public TestFlowDialog() : base(
+        new DialogMetadata(nameof(TestFlowDialog), "Test flow dialog", "1.0.0", true),
+        new IDialogPart[]
+        {
+            new MessageDialogPart("Welcome", "Welcome", "Welcome to the health advisor application. By answering questions, we can give you an advice how to improve your health. You can continue to start analyzing your health.", WelcomeGroup),
+            new SingleRequiredQuestionDialogPart
+            (
+                "Age",
+                "Age",
+                "How old are you?",
+                GetInformationGroup,
+                new IDialogPartResultDefinition[]
+                {
+                    new DialogPartResultDefinition("<10", "0 to 9 years old", ResultValueType.None),
+                    new DialogPartResultDefinition("10-19", "10 to 19 years old", ResultValueType.None),
+                    new DialogPartResultDefinition("20-29", "20 to 29 years old", ResultValueType.None),
+                    new DialogPartResultDefinition("30-39", "30 to 39 years old", ResultValueType.None),
+                    new DialogPartResultDefinition("40-49", "40 to 49 years old", ResultValueType.None),
+                    new DialogPartResultDefinition("50+", "Older than 50 years", ResultValueType.None),
+                }
+            ),
+            new AgeDecisionDialogPart("AgeDecision"),
+            new MessageDialogPart("TooYoung", "Completed", "Too bad, you are too young. We can't give advice on kids.", CompletedGroup),
+            new StaticNavigationDialogPart("TooYoungNavigation", EmailPartId),
+            new QuestionDialogPart
+            (
+                "SportsTypes",
+                "Sports types",
+                "What type of sports do you do?",
+                GetInformationGroup,
+                new IDialogPartResultDefinition[]
+                {
+                    new DialogPartResultDefinition("Bicycle", "Bicycle riding", ResultValueType.None),
+                    new DialogPartResultDefinition("Soccer", "Socced", ResultValueType.None),
+                    new DialogPartResultDefinition("Swimming", "Swimming", ResultValueType.None),
+                    new DialogPartResultDefinition("Aerobics", "Aerobics", ResultValueType.None),
+                    new DialogPartResultDefinition("Tennis", "Tennis", ResultValueType.None),
+                    new DialogPartResultDefinition("Baseball", "Baseball", ResultValueType.None),
+                    new DialogPartResultDefinition("Hockey", "Hockey", ResultValueType.None),
+                    new DialogPartResultDefinition("Other", "Other sports (please specify)", ResultValueType.Text),
+                }
+            ),
+            new SportsTypeDecisionDialogPart("SportsTypeDecision"),
+            new MessageDialogPart("Healthy", "Healthy", "You're all good! Keep up the good work.", CompletedGroup),
+            new StaticNavigationDialogPart("HealthyNavigation", EmailPartId),
+            new MessageDialogPart("Unhealthy", "Unhealthy", "Our advice: It's time to do some sports, mate!", CompletedGroup),
+            new StaticNavigationDialogPart("UnhealthyNavigation", EmailPartId),
+            new SingleOptionalQuestionDialogPart(EmailPartId, "E-mail address", "Thank you for using this application. You can leave your e-mail address in case you have comments or questions.", CompletedGroup, new[] { new DialogPartResultDefinition("EmailAddress", "E-mail address", ResultValueType.Text) })
+        },
+        new ErrorDialogPart("Error", "Something went wrong. Please try again, or contact us in case the problem persists.", null),
+        new AbortedDialogPart("Aborted", "The dialog is aborted. You can come back any time to start the application again."),
+        new CompletedDialogPart("Completed", "Completed", "Thank you for using this application. Please come back soon!", CompletedGroup),
+        new[] { WelcomeGroup, GetInformationGroup, CompletedGroup }
+        )
     {
-        var welcomeGroup = new DialogPartGroup("Welcome", "Welcome", 1);
-        var getInformationGroup = new DialogPartGroup("Get information", "Get information", 2);
-        var completedGroup = new DialogPartGroup("Completed", "Completed", 3);
-        var emailPart = new SingleOptionalQuestionDialogPart("Email", "E-mail address", "Thank you for using this application. You can leave your e-mail address in case you have comments or questions.", completedGroup, new[] { new DialogPartResultDefinition("EmailAddress", "E-mail address", ResultValueType.Text) });
-        Parts = new ValueCollection<IDialogPart>
-        (
-            new IDialogPart[]
-            {
-                new MessageDialogPart("Welcome", "Welcome", "Welcome to the health advisor application. By answering questions, we can give you an advice how to improve your health. You can continue to start analyzing your health.", welcomeGroup),
-                new SingleRequiredQuestionDialogPart
-                (
-                    "Age",
-                    "Age",
-                    "How old are you?",
-                    getInformationGroup,
-                    new IDialogPartResultDefinition[]
-                    {
-                        new DialogPartResultDefinition("<10", "0 to 9 years old", ResultValueType.None),
-                        new DialogPartResultDefinition("10-19", "10 to 19 years old", ResultValueType.None),
-                        new DialogPartResultDefinition("20-29", "20 to 29 years old", ResultValueType.None),
-                        new DialogPartResultDefinition("30-39", "30 to 39 years old", ResultValueType.None),
-                        new DialogPartResultDefinition("40-49", "40 to 49 years old", ResultValueType.None),
-                        new DialogPartResultDefinition("50+", "Older than 50 years", ResultValueType.None),
-                    }
-                ),
-                new AgeDecisionDialogPart("AgeDecision"),
-                new MessageDialogPart("TooYoung", "Completed", "Too bad, you are too young. We can't give advice on kids.", completedGroup),
-                new StaticNavigationDialogPart("TooYoungNavigation", emailPart.Id),
-                new QuestionDialogPart
-                (
-                    "SportsTypes",
-                    "Sports types",
-                    "What type of sports do you do?",
-                    getInformationGroup,
-                    new IDialogPartResultDefinition[]
-                    {
-                        new DialogPartResultDefinition("Bicycle", "Bicycle riding", ResultValueType.None),
-                        new DialogPartResultDefinition("Soccer", "Socced", ResultValueType.None),
-                        new DialogPartResultDefinition("Swimming", "Swimming", ResultValueType.None),
-                        new DialogPartResultDefinition("Aerobics", "Aerobics", ResultValueType.None),
-                        new DialogPartResultDefinition("Tennis", "Tennis", ResultValueType.None),
-                        new DialogPartResultDefinition("Baseball", "Baseball", ResultValueType.None),
-                        new DialogPartResultDefinition("Hockey", "Hockey", ResultValueType.None),
-                        new DialogPartResultDefinition("Other", "Other sports (please specify)", ResultValueType.Text),
-                    }
-                ),
-                new SportsTypeDecisionDialogPart("SportsTypeDecision"),
-                new MessageDialogPart("Healthy", "Healthy", "You're all good! Keep up the good work.", completedGroup),
-                new StaticNavigationDialogPart("HealthyNavigation", emailPart.Id),
-                new MessageDialogPart("Unhealthy", "Unhealthy", "Our advice: It's time to do some sports, mate!", completedGroup),
-                new StaticNavigationDialogPart("UnhealthyNavigation", emailPart.Id),
-                emailPart
-            }
-        );
-        ErrorPart = new ErrorDialogPart("Error", "Something went wrong. Please try again, or contact us in case the problem persists.", null);
-        AbortedPart = new AbortedDialogPart("Aborted", "The dialog is aborted. You can come back any time to start the application again.");
-        CompletedPart = new CompletedDialogPart("Completed", "Completed", "Thank you for using this application. Please come back soon!", completedGroup);
-        PartGroups = new ValueCollection<IDialogPartGroup>(new[] { welcomeGroup, getInformationGroup, completedGroup });
     }
-
-    public IEnumerable<IDialogPartResult> ReplaceAnswers(IEnumerable<IDialogPartResult> existingDialogPartResults,
-                                                         IEnumerable<IDialogPartResult> newDialogPartResults)
-    {
-        var dialogPartIds = newDialogPartResults.GroupBy(x => x.DialogPartId).Select(x => x.Key).ToArray();
-        return existingDialogPartResults.Where(x => !dialogPartIds.Contains(x.DialogPartId)).Concat(newDialogPartResults);
-    }
-
-    public IEnumerable<IDialogPartResult> ResetDialogPartResultByPart(IEnumerable<IDialogPartResult> existingDialogPartResults, IDialogPart currentPart)
-        => existingDialogPartResults.Where(x => x.DialogPartId != currentPart.Id);
-
-    public bool CanNavigateTo(IDialogPart currentPart, IDialogPart navigateToPart, IEnumerable<IDialogPartResult> existingDialogPartResults)
-        => currentPart.Id == navigateToPart.Id || existingDialogPartResults.Any(x => x.DialogPartId == navigateToPart.Id);
 }
 
 /*
