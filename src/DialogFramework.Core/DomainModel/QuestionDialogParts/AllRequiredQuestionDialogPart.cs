b34549a86a7c31720894801ac7f98aad092c37af
Paul Deen <paul.deen@dutchworkz.nl>
2022-04-30 11:05:19 +0200

    Added dialog part which ensures all parts of a question are answered

---
+++ src/DialogFramework.Core/DomainModel/QuestionDialogParts/AllRequiredQuestionDialogPart.cs
@@ -0,0 +1,23 @@
+﻿namespace DialogFramework.Core.DomainModel.QuestionDialogParts;
+
+public record AllRequiredQuestionDialogPart : QuestionDialogPart
+{
+    public AllRequiredQuestionDialogPart(string id,
+                                         string heading,
+                                         string title,
+                                         IDialogPartGroup group,
+                                         IEnumerable<IDialogPartResultDefinition> results)
+        : base(id, heading, title, group, results)
+    {
+    }
+
+    protected override void HandleValidate(IEnumerable<IDialogPartResult> dialogPartResults)
+    {
+        base.HandleValidate(dialogPartResults);
+        var partCount = dialogPartResults.Where(x => !string.IsNullOrEmpty(x.Result.Id)).GroupBy(x => x.DialogPart.Id).Count();
+        if (partCount != Results.Count)
+        {
+            ErrorMessages.Add($"All {Results.Count} answers are required");
+        }
+    }
+}
