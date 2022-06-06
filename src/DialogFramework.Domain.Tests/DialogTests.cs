b0fb47d1c87a415b8a4bf8308c87948a8781f47e
Paul Deen <paul.deen@dutchworkz.nl>
2022-06-06 16:41:06 +0200

    Added some tests for Dialog class

--- src/DialogFramework.Domain.Tests/DialogTests.cs
+++ src/DialogFramework.Domain.Tests/DialogTests.cs
@@ -3,68 +3,217 @@
 public class DialogTests
 {
     private static string Id => Guid.NewGuid().ToString();
+    private readonly Mock<IConditionEvaluator> _conditionEvaluatorMock;
+
+    public DialogTests()
+    {
+        _conditionEvaluatorMock = new Mock<IConditionEvaluator>();
+    }
 
     [Fact]
     public void CanAbort_Returns_False_When_CurrentPart_Is_AbortedPart()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.AbortedPart, DialogState.Aborted);
+
+        // Act
+        var actual = sut.CanAbort(dialogDefinition);
+
+        // Assert
+        actual.Should().BeFalse();
     }
 
     [Fact]
     public void CanAbort_Returns_False_When_CurrentState_Is_Not_InProgess()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.CompletedPart, DialogState.Completed);
+
+        // Act
+        var actual = sut.CanAbort(dialogDefinition);
+
+        // Assert
+        actual.Should().BeFalse();
     }
 
     [Fact]
     public void CanAbort_Returns_True_When_CurrentState_Is_InProgess_And_CurrentPart_Is_Not_AbortedPart()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.Parts.First(), DialogState.InProgress);
+
+        // Act
+        var actual = sut.CanAbort(dialogDefinition);
+
+        // Assert
+        actual.Should().BeTrue();
     }
 
     [Fact]
     public void Abort_Throws_When_CanAbort_Is_False()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.CompletedPart, DialogState.Completed);
+
+        // Act
+        var act = new Action(() => sut.Abort(dialogDefinition));
+
+        // Assert
+        act.Should().ThrowExactly<InvalidOperationException>();
     }
 
     [Fact]
     public void Abort_Updates_State_Correcly_When_CanAbort_Is_True()
     {
-        //verify CurrentPartId, CurrentGroupId and State
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.Parts.First(), DialogState.InProgress);
+
+        // Act
+        sut.Abort(dialogDefinition);
+
+        // Assert
+        sut.CurrentPartId.Should().Be(dialogDefinition.AbortedPart.Id);
+        sut.CurrentGroupId.Should().BeNull();
+        sut.CurrentState.Should().Be(DialogState.Aborted);
+
     }
 
     [Fact]
     public void CanContinue_Returns_False_When_CurrentState_Is_Not_InProgress()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.CompletedPart, DialogState.Completed);
+
+        // Act
+        var actual = sut.CanContinue(dialogDefinition, Enumerable.Empty<IDialogPartResult>());
+
+        // Assert
+        actual.Should().BeFalse();
     }
 
     [Fact]
     public void CanContinue_Returns_True_When_CurrentState_Is_InProgress()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.Parts.First(), DialogState.InProgress);
+
+        // Act
+        var actual = sut.CanContinue(dialogDefinition, Enumerable.Empty<IDialogPartResult>());
+
+        // Assert
+        actual.Should().BeTrue();
     }
 
     [Fact]
     public void Continue_Throws_When_CanContinue_Is_False()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create(Id, dialogDefinition.Metadata, dialogDefinition.CompletedPart, DialogState.Completed);
+
+        // Act
+        var act = new Action(() => sut.Continue(dialogDefinition, Enumerable.Empty<IDialogPartResult>(), _conditionEvaluatorMock.Object));
+
+        // Assert
+        act.Should().ThrowExactly<InvalidOperationException>();
     }
 
     [Fact]
     public void Continue_Updates_State_Correctly_When_CanContinue_Is_True()
     {
-        //verify CurrentPartId (which is nextpartid), CurrentGroupId and CurrentState (based on next part), merged results and validation results (replaced)
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create
+        (
+            Id,
+            dialogDefinition.Metadata,
+            dialogDefinition.Parts.First(),
+            new[]
+            {
+                new DialogPartResultBuilder()
+                    .WithDialogPartId(new DialogPartIdentifierBuilder(dialogDefinition.Parts.First().Id))
+                    .WithResultId(new DialogPartResultIdentifierBuilder(dialogDefinition.Parts.OfType<IQuestionDialogPart>().First().Results.First().Id))
+                    .WithValue(new YesNoDialogPartResultValueBuilder().WithValue(true))
+                    .Build()
+            },
+            new[]
+            {
+                new DialogValidationResultBuilder()
+                    .WithErrorMessage("You fool! You provided the wrong input")
+                    .Build()
+            }
+        );
+
+        // Act
+        sut.Continue
+        (
+            dialogDefinition,
+            new[]
+            {
+                new DialogPartResultBuilder()
+                    .WithDialogPartId(new DialogPartIdentifierBuilder(dialogDefinition.Parts.First().Id))
+                    .WithResultId(new DialogPartResultIdentifierBuilder(dialogDefinition.Parts.OfType<IQuestionDialogPart>().First().Results.First().Id))
+                    .WithValue(new YesNoDialogPartResultValueBuilder().WithValue(false))
+                    .Build()
+            }, _conditionEvaluatorMock.Object);
+
+        // Assert
+        var nextPart = dialogDefinition.Parts.Skip(1).First();
+        sut.CurrentPartId.Should().Be(nextPart.Id);
+        sut.CurrentGroupId.Should().Be(nextPart.GetGroupId());
+        sut.CurrentState.Should().Be(nextPart.GetState());
+        sut.ValidationErrors.Should().BeEmpty(); //cleared
+        //first result value (true) is replaced with second (false)
+        sut.Results.Should().ContainSingle();
+        sut.Results.Single().Value.Value.Should().BeEquivalentTo(false);
     }
 
     [Fact]
     public void Error_Updates_State_Correctly()
     {
-        //verify CurrentPartId (which is Errorpart.Id), CurrentGroupId and CurrentState (based on Errorpart), Errors (overwritten with errors argument) and ValidationErrors (cleared)
-        throw new NotImplementedException();
+        // Arrange
+        var dialogDefinition = DialogDefinitionFixture.CreateBuilder().Build();
+        var sut = DialogFixture.Create
+        (
+            Id,
+            dialogDefinition.Metadata,
+            dialogDefinition.Parts.First(),
+            new[]
+            {
+                new DialogPartResultBuilder()
+                    .WithDialogPartId(new DialogPartIdentifierBuilder(dialogDefinition.Parts.First().Id))
+                    .WithResultId(new DialogPartResultIdentifierBuilder(dialogDefinition.Parts.OfType<IQuestionDialogPart>().First().Results.First().Id))
+                    .WithValue(new YesNoDialogPartResultValueBuilder().WithValue(true))
+                    .Build()
+            },
+            new[]
+            {
+                new DialogValidationResultBuilder()
+                    .WithErrorMessage("You fool! You provided the wrong input")
+                    .Build()
+            },
+            new[]
+            {
+                new ErrorBuilder().WithMessage("Kaboom").Build()
+            }
+        );
+        // Act
+        sut.Error(dialogDefinition, new Error("Something went wrong"));
+
+        // Assert
+        sut.CurrentPartId.Should().BeEquivalentTo(dialogDefinition.ErrorPart.Id);
+        sut.CurrentGroupId.Should().BeEquivalentTo(dialogDefinition.ErrorPart.GetGroupId());
+        sut.CurrentState.Should().Be(DialogState.ErrorOccured);
+        sut.Errors.Should().BeEquivalentTo(new[] { new Error("Something went wrong") });
+        sut.ValidationErrors.Should().BeEmpty(); // cleared
+        sut.Results.Should().ContainSingle(); // not overwritten
     }
 
     [Fact]
