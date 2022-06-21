8314603ec0030ac7104fb198f99e5dd731238ef9
Paul Deen <paul.deen@dutchworkz.nl>
2022-06-21 16:42:44 +0200

    Refactor: Made arguments on step bindings strongly typed

--- src/DialogFramework.SpecFlow.Tests/StepDefinitions/DialogStepDefinitions.cs
+++ src/DialogFramework.SpecFlow.Tests/StepDefinitions/DialogStepDefinitions.cs
@@ -14,19 +14,19 @@ public sealed class DialogStepDefinitions
         => _lastResult = ApplicationEntrypoint.Instance.Continue(GetCurrentDialog(), answers.Select(x => x.Build()));
 
     [Then("the current state should be (.*)")]
-    public void ThenTheCurrentStateShouldBe(string result)
-        => GetCurrentDialog().CurrentState.ToString().Should().Be(result);
+    public void ThenTheCurrentStateShouldBe(DialogState state)
+        => GetCurrentDialog().CurrentState.Should().Be(state);
 
     [When(@"I answer '([^']*)' for result '([^']*)'")]
-    public void WhenIAnswerForResult(string value, string result)
+    public void WhenIAnswerForResult(string value, IDialogPartResultIdentifier result)
         => Answer(result, value);
 
     [When(@"I answer No for result '([^']*)'")]
-    public void WhenIAnswerNoForResult(string result)
+    public void WhenIAnswerNoForResult(IDialogPartResultIdentifier result)
         => Answer(result, false);
 
     [When(@"I answer Yes for result '([^']*)'")]
-    public void WhenIAnswerYesForResult(string result)
+    public void WhenIAnswerYesForResult(IDialogPartResultIdentifier result)
         => Answer(result, true);
 
     private IDialog GetCurrentDialog()
@@ -38,12 +38,12 @@ public sealed class DialogStepDefinitions
         return _lastResult!.GetValueOrThrow($"Last result was not successful. Details: {_lastResult}");
     }
     
-    private void Answer(string result, object? value)
+    private void Answer(IDialogPartResultIdentifier resultId, object? value)
     {
         var results = new[]
         {
             new DialogPartResultAnswerBuilder()
-                .WithResultId(new DialogPartResultIdentifierBuilder().WithValue(result))
+                .WithResultId(new DialogPartResultIdentifierBuilder(resultId))
                 .WithValue(new DialogPartResultValueAnswerBuilder().WithValue(value))
                 .Build()
         };
