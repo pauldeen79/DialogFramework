ca49d906ed157dc12f780e801073e2da23e8c245
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-02 20:36:02 +0200

    Added first tests for model

---
+++ src/DialogFramework.Domain.Tests/DialogTests.cs
@@ -0,0 +1,24 @@
+﻿namespace DialogFramework.Domain.Tests;
+
+public class DialogTests
+{
+    [Fact]
+    public void Constructing_Dialog_With_Null_Id_Throws()
+    {
+        // Arrange
+        var builder = new DialogBuilder().WithId(default(string)!);
+
+        // Act
+        builder.Invoking(x => x.Build()).Should().Throw<ValidationException>();
+    }
+
+    [Fact]
+    public void Constructing_Dialog_With_Empty_Id_Throws()
+    {
+        // Arrange
+        var builder = new DialogBuilder().WithId(string.Empty);
+
+        // Act
+        builder.Invoking(x => x.Build()).Should().Throw<ValidationException>();
+    }
+}
--- src/DialogFramework.Domain.Tests/Usings.cs
+++ src/DialogFramework.Domain.Tests/Usings.cs
@@ -1,7 +1,4 @@
 ﻿global using System.ComponentModel.DataAnnotations;
-global using CrossCutting.Common.Extensions;
-global using CrossCutting.Common.Results;
-global using CrossCutting.Common.Testing;
+global using DialogFramework.Domain.Builders;
 global using FluentAssertions;
-global using Moq;
 global using Xunit;
