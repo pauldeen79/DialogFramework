20b13ef406a98c86c94886fe52c22c14331f644b
Paul Deen <pauldeen@gmx.li>
2024-05-07 10:19:33 +0200

    Fixed validation

--- src/DialogFramework.CodeGeneration/CodeGenerationProviders/DialogFrameworkCSharpClassBase.cs
+++ src/DialogFramework.CodeGeneration/CodeGenerationProviders/DialogFrameworkCSharpClassBase.cs
@@ -17,6 +17,7 @@ public abstract class DialogFrameworkCSharpClassBase : CsharpClassGeneratorPipel
     protected override string CoreNamespace => $"{ProjectName}.Domain";
     protected override bool CreateRecord => true;
     protected override bool AddBackingFields => true;
+    protected override bool CopyAttributes => true;
     protected override SubVisibility SetterVisibility => SubVisibility.Private;
 
     protected override bool IsAbstractType(Type type)
